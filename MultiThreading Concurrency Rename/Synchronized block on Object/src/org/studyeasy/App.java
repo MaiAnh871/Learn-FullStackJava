d23a9f4a59507b91a0fafd79fae076c2e2f497b1
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-10-13 15:15:40 +0700

    push Synchronized block on Object

---
+++ MultiThreading Concurrency Rename/Synchronized block on Object/src/org/studyeasy/App.java
@@ -0,0 +1,63 @@
+package org.studyeasy;
+
+// We dont want to synchronized the complete method.
+class Brakets {
+
+    private Object lock = "lock";
+    // This lock is specific to object
+    public void generate() {
+        synchronized (lock) {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (i <= 5) {
+                    System.out.print("[");
+                } else {
+                    System.out.print("]");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int j = 0; j <= 10; j++) {
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+        Brakets braket = new Brakets();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                for (int i = 1; i <= 5; i++) {
+                    braket.generate();
+                }
+                long endTime = System.currentTimeMillis();
+                System.out.println("Time requied for thread 1 was: " + (endTime - startTime));
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                for (int i = 1; i <= 5; i++) {
+                    braket.generate();
+                }
+                long endTime = System.currentTimeMillis();
+                System.out.println("Time requied for thread 2 was: " + (endTime - startTime));
+            }
+        }).start();
+    }
+}
