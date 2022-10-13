d36bacebe0b2f938d1f935e8e109e025fe716720
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-10-13 16:31:54 +0700

    push Volatile keyword

---
+++ MultiThreading Concurrency Rename/Volatile keyword/src/org/studyeasy/App.java
@@ -0,0 +1,34 @@
+package org.studyeasy;
+
+public class App {
+
+    volatile public static int flag = 0;
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (flag == 0) {
+                        System.out.println("Running");
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                App.flag = 1;
+                System.out.println("Flag value updated");
+            }
+        }).start();
+    }
+}
