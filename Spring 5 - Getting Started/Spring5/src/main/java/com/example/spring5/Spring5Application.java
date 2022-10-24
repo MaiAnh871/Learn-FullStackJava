997e513ce5d753fc8eaf1f182bb1f9dbec987274
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-10-24 13:59:12 +0700

    push Spring 5 Getting Started

---
+++ Spring 5 - Getting Started/Spring5/src/main/java/com/example/spring5/Spring5Application.java
@@ -0,0 +1,10 @@
+package com.example.spring5;
+
+
+public class Spring5Application {
+
+    public static void main(String[] args) {
+        Spring5Application.run(Spring5Application.class, args);
+    }
+
+}
